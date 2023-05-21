c95f72b13be6dacf853a7ccc608e0c84f377c7f6
Suzana conceição de sousa <106107598+SuzanaSousa@users.noreply.github.com>
2023-05-21 12:31:27 -0300

    Add files via upload
    Agregação

---
+++ AgregacaoContaVetor/Program.cs
@@ -0,0 +1,18 @@
+﻿//função Main () implicido
+using AgregacaoContaVetor;
+
+Conta c1 = new Conta();
+c1.Numero = 3;
+c1.Saldo = 3000;
+
+Cliente cli1 = new Cliente("Lia",2222,22);
+Cliente cli2 = new Cliente("Caio",4444,44);
+Cliente cli3 = new Cliente ("Bob",5555,55);
+
+c1.VetTitular = new List<Cliente>(); //primeiro estência para depois 
+c1.VetTitular.Add(cli1);
+c1.VetTitular.Add(cli2);
+c1.VetTitular.Add(cli3);
+
+foreach(Cliente c in c1.VetTitular)
+    c.MostrarCliente();
